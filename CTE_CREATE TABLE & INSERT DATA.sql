5939ff096c0c20d3debcccd0b052ce9aa45ef221
godtechwak <68718384+godtechwak@users.noreply.github.com>
2020-08-08 05:09:54 +0900

    CREATE TABLE & INSERT DATA Script

---
+++ CTE_CREATE TABLE & INSERT DATA.sql
@@ -0,0 +1,31 @@
+CREATE TABLE _ErrorTable
+(
+	CompanySeq	 INT
+   ,OrgType		 INT
+   ,DeptSeq		 INT
+   ,Seq			 INT
+   ,UppDeptSeq	 INT
+   ,BegDate		 NCHAR(8)
+   ,EndDate		 NCHAR(8)
+   ,UMDeptLevel	 INT
+   ,DispSeq		 INT
+   ,Remark		 NVARCHAR(100)
+   ,LastUserSeq	 INT
+   ,LastDateTime DATETIME
+)
+
+
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,0,1,2,N'20200702',N'99991231',3054003,6,N'',1,'2020-07-28 13:58:44.497'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,1,1,0,N'20090101',N'99991231',3054001,1,N'',1,'2020-07-28 13:58:44.503'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,2,1,1,N'20100413',N'99991231',3054002,1,N'',1,'2020-07-28 13:58:44.510'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,5,1,4,N'20100413',N'20120712',3054004,1,N'',1,'2020-07-28 13:58:44.513'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,17,1,2,N'20120713',N'99991231',3054003,1,N'',1,'2020-07-28 13:58:44.517'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,18,1,16,N'20120713',N'20120819',3054004,1,N'',1,'2020-07-28 13:58:44.520'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,18,2,2,N'20120820',N'99991231',3054003,4,N'',1,'2020-07-28 13:58:44.523'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,20,1,62,N'20170201',N'99991231',3054005,1,N'',1,'2020-07-28 13:58:44.530'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,21,1,62,N'20170201',N'99991231',3054005,2,N'',1,'2020-07-28 13:58:44.533'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,24,1,61,N'20120713',N'99991231',3054004,3,N'',1,'2020-07-28 13:58:44.537'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,50,1,2,N'20130101',N'99991231',3054003,2,N'',1,'2020-07-28 13:58:44.540'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,60,1,61,N'20130101',N'99991231',3054004,2,N'',65,'2020-07-28 13:58:44.543'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,61,1,2,N'20130101',N'99991231',3054003,5,N'',65,'2020-07-28 13:58:44.550'
+Insert Into _ErrorTable(CompanySeq,OrgType,DeptSeq,Seq,UppDeptSeq,BegDate,EndDate,UMDeptLevel,DispSeq,Remark,LastUserSeq,LastDateTime) select 1,1,62,1,61,N'20170201',N'99991231',3054004,1,N'',1,'2020-07-28 13:58:44.553'
